2731374f884857b8d3cc234f1b8580026fd688b9
Kislichenko Bogdan <bogdan600@yandex.ru>
2020-09-20 12:35:42 +0300

    add tmp clients to test postgres db

---
+++ src/main/java/com/kislichenko/news/Client.java
@@ -0,0 +1,24 @@
+package com.kislichenko.news;
+
+import lombok.Data;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "clients")
+@Data
+public class Client {
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "phone")
+    private String phone;
+}
---
+++ src/main/java/com/kislichenko/news/ClientServiceImpl.java
@@ -0,0 +1,46 @@
+package com.kislichenko.news;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ClientServiceImpl{
+
+    private final ClientRepository clientRepository;
+
+    public ClientServiceImpl(ClientRepository clientRepository) {
+        this.clientRepository = clientRepository;
+    }
+
+    public void create(Client client) {
+        clientRepository.save(client);
+    }
+
+    public List<Client>  readAll() {
+        return clientRepository.findAll();
+    }
+
+    public Client read(int id) {
+        return clientRepository.getOne(id);
+    }
+
+    public boolean update(Client client, int id) {
+        if (clientRepository.existsById(id)) {
+            client.setId(id);
+            clientRepository.save(client);
+            return true;
+        }
+
+        return false;
+    }
+
+    public boolean delete(int id) {
+        if (clientRepository.existsById(id)) {
+            clientRepository.deleteById(id);
+            return true;
+        }
+        return false;
+    }
+}
