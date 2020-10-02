87de0f2da0ac30983bf2f7e7f245029bb2d89cc0
Kislichenko Bogdan <bogdan600@yandex.ru>
2020-10-02 13:15:11 +0300

    init home-advertiser, request-page components and add simple routing

--- news-front/src/app/app-routing.module.ts
+++ news-front/src/app/app-routing.module.ts
@@ -1,8 +1,22 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {MainLayoutComponent} from './shared/components/main-layout/main-layout.component';
+import {HomeAdvertiserComponent} from './home-advertiser/home-advertiser.component';
+import {RequestPageComponent} from './request-page/request-page.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    //сначала мы загружаем MainLayoutComponent. Чтобы загрузить HomeAdvertiserComponent,
+    //делаем редирект. pathMatch - путь должен полностью совпадать с родительским.
+    path: '', component: MainLayoutComponent, children: [
+      {path: '', redirectTo: '/', pathMatch: 'full'},
+      {path: '', component: HomeAdvertiserComponent},
+      {path: 'request/:id', component: RequestPageComponent}
+
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- news-front/src/app/app.module.ts
+++ news-front/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainLayoutComponent } from './shared/components/main-layout/main-layout.component';
+import { HomeAdvertiserComponent } from './home-advertiser/home-advertiser.component';
+import { RequestPageComponent } from './request-page/request-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainLayoutComponent,
+    HomeAdvertiserComponent,
+    RequestPageComponent
   ],
   imports: [
     BrowserModule,
