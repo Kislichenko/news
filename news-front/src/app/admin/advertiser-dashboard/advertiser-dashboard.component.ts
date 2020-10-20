9d98f69de2b6281e9fad045feb3ed88b02e42247
Kislichenko Bogdan <bogdan600@yandex.ru>
2020-10-20 12:23:16 +0300

    advertiser dashboard

--- news-front/src/app/admin/advertiser-dashboard/advertiser-dashboard.component.ts
+++ news-front/src/app/admin/advertiser-dashboard/advertiser-dashboard.component.ts
@@ -1,22 +1,39 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 import {AuthService} from '../shared/services/auth.service';
+import {ReqdataService} from '../../shared/reqdata.service';
+import {ReqData} from '../../shared/interfaces';
+import {Subscription} from 'rxjs';
 
 @Component({
   selector: 'app-advertiser-dashboard',
   templateUrl: './advertiser-dashboard.component.html',
   styleUrls: ['./advertiser-dashboard.component.scss']
 })
-export class AdvertiserDashboardComponent implements OnInit {
+export class AdvertiserDashboardComponent implements OnInit, OnDestroy {
+
+  reqDatas: ReqData[] = []
+  pSub: Subscription
 
   constructor(
-    private auth: AuthService
+    private auth: AuthService,
+    private reqdataService: ReqdataService
   ) { }
 
   ngOnInit(): void {
+    this.pSub = this.reqdataService.getAll().subscribe(reqDatas => {
+      this.reqDatas = reqDatas
+    })
   }
 
-  test(){
-    console.log(this.auth.token)
+  ngOnDestroy(): void {
+    //для того, чтобы не было утечек памяти, нужно очищать подписку
+    if(this.pSub){
+      this.pSub.unsubscribe()
+    }
+  }
+
+  remove(id: string){
+
   }
 
 }
--- news-front/src/app/shared/reqdata.service.ts
+++ news-front/src/app/shared/reqdata.service.ts
@@ -3,6 +3,7 @@ import {HttpClient} from '@angular/common/http';
 import {Observable} from 'rxjs';
 import {ReqData} from './interfaces';
 import {environment} from '../../environments/environment';
+import {map, tap} from 'rxjs/operators';
 
 @Injectable({providedIn: 'root'})
 export class ReqdataService{
@@ -12,5 +13,20 @@ export class ReqdataService{
 
   create(reqData: ReqData):Observable<ReqData>{
     return this.http.post<ReqData>(`${environment.backendUrl}/requests`, reqData)
+  }
+
+  getAll(): Observable<ReqData[]>{
+    return this.http.get(`${environment.backendUrl}/requests`)
+      .pipe(
+        tap(response => {
+          console.log(response)
+        }),
+        map( (response: {[key: string]: any}) =>{
+          console.log(response[1])
+        return Object.keys(response).map(key => ({
+          ...response[key],
+          id: key
+        }))
+      }))
   }
 }
