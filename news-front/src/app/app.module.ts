ca0b949de2a9c256484ce13f5fa2205b1e632112
Kislichenko Bogdan <bogdan600@yandex.ru>
2020-10-20 16:39:37 +0300

    add locale

--- news-front/src/app/app.module.ts
+++ news-front/src/app/app.module.ts
@@ -10,6 +10,10 @@ import { PostComponent } from './shared/components/post/post.component';
 import {SharedModule} from './shared/shared.module';
 import {HTTP_INTERCEPTORS} from '@angular/common/http';
 import {AuthInteceptor} from './shared/auth.inteceptor';
+import {registerLocaleData} from '@angular/common';
+import ruLocale from '@angular/common/locales/ru';
+
+registerLocaleData(ruLocale, 'ru')
 
 const INTERCEPTOR_PROVIDER: Provider = {
   provide: HTTP_INTERCEPTORS,
