4bcf04f00dfd7d87b59359c0744094c87c931121
Kislichenko Bogdan <bogdan600@yandex.ru>
2020-12-15 19:54:52 +0300

    add pay button for user and confirm button for admanager

--- news-front/src/app/admin/req-dashboard/req-dashboard.component.ts
+++ news-front/src/app/admin/req-dashboard/req-dashboard.component.ts
@@ -2,7 +2,7 @@ import {Component, OnDestroy, OnInit} from '@angular/core';
 import {AuthService} from '../shared/services/auth.service';
 import {ReqdataService} from '../../shared/reqdata.service';
 import {ReqData, Role} from '../../shared/interfaces';
-import {Observable, Subscription} from 'rxjs';
+import {Subscription} from 'rxjs';
 import {AlertService} from '../shared/services/alert.service';
 
 @Component({
@@ -12,42 +12,43 @@ import {AlertService} from '../shared/services/alert.service';
 })
 export class ReqDashboardComponent implements OnInit, OnDestroy {
 
-  reqDatas: ReqData[] = []
-  pSub: Subscription
-  dSub: Subscription
-  uSub: Subscription
-  searchStr = ''
+  reqDatas: ReqData[] = [];
+  pSub: Subscription;
+  dSub: Subscription;
+  uSub: Subscription;
+  searchStr = '';
 
   constructor(
     private auth: AuthService,
     private reqdataService: ReqdataService,
     private alertService: AlertService
-  ) { }
+  ) {
+  }
 
   ngOnInit(): void {
     this.pSub = this.reqdataService.getAll().subscribe(reqDatas => {
-      this.reqDatas = reqDatas
-    })
+      this.reqDatas = reqDatas;
+    });
   }
 
   ngOnDestroy(): void {
     //для того, чтобы не было утечек памяти, нужно очищать подписку
-    if(this.pSub){
-      this.pSub.unsubscribe()
+    if (this.pSub) {
+      this.pSub.unsubscribe();
     }
-    if(this.dSub){
-      this.dSub.unsubscribe()
+    if (this.dSub) {
+      this.dSub.unsubscribe();
     }
-    if(this.uSub){
-      this.uSub.unsubscribe()
+    if (this.uSub) {
+      this.uSub.unsubscribe();
     }
   }
 
-  remove(id: string){
+  remove(id: string) {
     this.dSub = this.reqdataService.remove(id).subscribe(() => {
-      this.reqDatas = this.reqDatas.filter(reqData => reqData.id !== id)
-      this.alertService.warning('Пост был удален!')
-    })
+      this.reqDatas = this.reqDatas.filter(reqData => reqData.id !== id);
+      this.alertService.warning('Пост был удален!');
+    });
   }
 
 
@@ -56,9 +57,19 @@ export class ReqDashboardComponent implements OnInit, OnDestroy {
       ...reqData,
       payed: true
     }).subscribe(() => {
-      this.alertService.success('Блок был оплачен')
+      this.alertService.success('Блок был оплачен');
       this.ngOnInit();
-    })
+    });
+  }
+
+  confirm($event: MouseEvent, reqData: ReqData) {
+    this.uSub = this.reqdataService.update({
+      ...reqData,
+      confirm: true
+    }).subscribe(() => {
+      this.alertService.success('Заявка на рекламу была подтверждена');
+      this.ngOnInit();
+    });
   }
 
   public get role(): typeof Role {
